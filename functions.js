f7a390a9d6bcfee012d840bc11da7011140b40f8
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2018-12-16 21:21:23 +0100

    final - second

--- functions.js
+++ functions.js
@@ -22,10 +22,11 @@ function insertSign() {
 
 function checkWinner() {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0].innerHTML === arr[i][1].innerHTML && arr[i][0].innerHTML === arr[i][2].innerHTML && arr[i][0].innerHTML !== "") {
-      arr[i][0].style.background = "red";
-      arr[i][1].style.background = "red";
-      arr[i][2].style.background = "red";
+    let line = arr[i];
+    if (line[0].innerHTML === line[1].innerHTML && line[0].innerHTML === line[2].innerHTML && line[0].innerHTML !== "") {
+      line[0].style.background = "red";
+      line[1].style.background = "red";
+      line[2].style.background = "red";
       for (let i = 0; i < field.length; i++) {
         field[i].removeEventListener("click", insertSign);
       }
